dda7196e4216524c3633781e4a6d12094a8f4313
phptuts <glaserpower@gmail.com>
2023-11-05 18:32:38 -0800

    7-register-form-plumbing

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function onEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function onSubmit() {
+    const registerFormObj = { email, password };
+    console.log(registerFormObj);
+  }
+
   return (
     <>
       <div className="row">
@@ -8,23 +21,37 @@ const Register = () => {
       </div>
       <div className="row">
         <div className="col">
-          <label for="email" className="form-label">
+          <label htmlFor="email" className="form-label">
             Email
           </label>
-          <input type="email" id="email" className="form-control" />
+          <input
+            type="email"
+            value={email}
+            onChange={onEmailChange}
+            id="email"
+            className="form-control"
+          />
         </div>
       </div>
       <div className="row">
         <div className="col">
-          <label for="inputPassword5" className="form-label">
+          <label htmlFor="inputPassword5" className="form-label">
             Password
           </label>
-          <input type="password" id="inputPassword5" className="form-control" />
+          <input
+            type="password"
+            value={password}
+            id="inputPassword5"
+            className="form-control"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
       </div>
       <div className="row mt-3">
         <div className="col">
-          <button className="btn btn-primary w-100">Register</button>
+          <button onClick={onSubmit} className="btn btn-primary w-100">
+            Register
+          </button>
         </div>
       </div>
     </>
