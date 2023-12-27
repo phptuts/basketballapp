c5eca292a37f34bf4d0a52833252449038b404f4
phptuts <glaserpower@gmail.com>
2023-12-26 22:00:45 -0800

    handling connection in game provider

--- src/contexts/game.context.jsx
+++ src/contexts/game.context.jsx
@@ -1,16 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 
 export const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
   const [updatedGame, setUpdateGame] = useState(null);
-  const ws = new WebSocket(import.meta.env.VITE_WEB_SOCKET_URL);
-  ws.onopen = function () {
-    console.log("open connection");
-  };
-  ws.onmessage = function (ev) {
-    setUpdateGame(JSON.parse(ev.data));
-  };
+  const ws = useRef(null);
+
+  useEffect(() => {
+    if (ws.current == null || ws.current.readyState != WebSocket.OPEN) {
+      ws.current = new WebSocket(import.meta.env.VITE_WEB_SOCKET_URL);
+    }
+    ws.current.onopen = function () {
+      console.log("open connection");
+    };
+    ws.current.onmessage = function (ev) {
+      console.log("message received");
+      setUpdateGame(JSON.parse(ev.data));
+    };
+
+    return () => {
+      ws.current?.close();
+    };
+  }, []);
+
   return (
     <GameContext.Provider value={updatedGame}>{children}</GameContext.Provider>
   );
